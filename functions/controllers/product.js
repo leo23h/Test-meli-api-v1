5357acff969b61be5b0b9d89477be694b065573d
Leonardo H <leo.23h1991@gmail.com>
2022-08-02 16:32:58 -0500

    create endpoints search product and get prod id

--- functions/controllers/product.js
+++ functions/controllers/product.js
@@ -1,13 +1,49 @@
 const axios = require('axios');
+const url = require('url');
+const querystring = require('querystring');
 
 exports.searchProduct = (req, res) => {
     (async () => {
         try {
-            let criterialSearch = req.params.q;
-            let response = await axios.get(`https://api.mercadolibre.com/sites/MLA/search?q=${criterialSearch}`);
-            return res.status(200).json({ status: "succeed", code: 200, message: "products found successfully!", data: response });
+            let objResp = {};
+            let urlqs = `https://api.mercadolibre.com/sites/MLA/search?q=${encodeURIComponent(req.query.q)}`;
+            console.log("criteria search1", decodeURIComponent(urlqs));
+            axios.get(urlqs).then((response) => {
+                // console.log("response search", response);
+                let itemsTemp = [];
+                objResp = {
+                    'author': {
+                        'name': 'Leonardo',
+                        'lastname': 'Herrera',
+                    },
+                    'categories': [],
+                    'items': []
+                }
+
+                // capture only 4 items by search
+                for (let i = 0; i <= 3; i++) {
+                    // CONDITIONS
+                    // new
+                    // not_specified
+                    // used
+                    itemsTemp.push({
+                        "id": response.data.results[i].id, 
+                        "title": response.data.results[i].title, 
+                        "price": {
+                            "currency": response.data.results[i].currency_id, 
+                            "amount": response.data.results[i].price, 
+                            "decimals": null
+                        },
+                        "picture": response.data.results[i].thumbnail, 
+                        "condition": response.data.results[i].condition, 
+                        "free_shipping": response.data.results[i].shipping.free_shipping
+                    });
+                }
+                objResp.items = itemsTemp;
+
+                return res.status(200).json({ status: "succeed", code: 200, message: "products found successfully!", data: objResp });
+            })
         } catch (error) {
-            functions.logger.error(error)
             return res.status(500).json({ status: "error", code: 500, message: "Error to found products", data: error });
         }
     })();
@@ -17,25 +53,39 @@ exports.getProductById = (req, res) => {
     (async () => {
         try {
             let id = req.params.id;
-            let response = await axios.get(`https://api.mercadolibre.com/items/​${id}`);
-            return res.status(200).json({ status: "succeed", code: 200, message: "producty by id successfully!", data: response });
+            let objResponse = {};
+            console.log("get product by id", id)
+            let responseItem = await axios.get(`https://api.mercadolibre.com/items/${id}`);
+            let responseDescription = await axios.get(`https://api.mercadolibre.com/items/${id}/description`);
+            // Format data
+            objResponse = {
+                "author": {
+                    "name": "Leonardo",
+                    "lastname": "Herrera" 
+                },
+                "item": {
+                    "id": responseItem.data.id, 
+                    "title": responseItem.data.title, 
+                    "price": {
+                        "currency": responseItem.data.currency_id, 
+                        "amount": responseItem.data.price, 
+                        "decimals": null,
+                    },
+                    "picture": responseItem.data.pictures[0].secure_url,
+                    "condition": responseItem.data.condition, 
+                    "free_shipping": responseItem.data.shipping.free_shipping, 
+                    "sold_quantity": responseItem.data.sold_quantity,
+                    "description": responseDescription.data.plain_text
+                } 
+            }
+                
+
+
+            return res.status(200).json({ status: "succeed", code: 200, message: "producty by id successfully!", data: objResponse });
         } catch (error) {
-            functions.logger.error(error)
             return res.status(500).json({ status: "error", code: 500, message: "Error to found products", data: error });
         }
     })();
 }
 
-exports.getDescriptionProductById = (req, res) => {
-    (async () => {
-        try {
-            let id = req.params.id;
-            let response = await axios.get(`https://api.mercadolibre.com/items/​${id}/description`);
-            return res.status(200).json({ status: "succeed", code: 200, message: "description product successfully!", data: response });
-        } catch (error) {
-            functions.logger.error(error)
-            return res.status(500).json({ status: "error", code: 500, message: "Error to found products", data: error });
-        }
-    })();
-}
 
--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const corsConfig = {
     credentials: true,
@@ -10,6 +13,13 @@ app.use(cors(corsConfig));
 
 const productModule = require("./controllers/product");
 
-app.get('/items', productModule.searchProduct);
-app.get('/items/​:id', productModule.getDescriptionProductById);
-app.get('/item/​:id', productModule.getProductById);
+app.get('/api/items', productModule.searchProduct);
+app.get('/api/items/:id', productModule.getProductById);
+
+const server = app.listen(3000, () => console.log('Server ready'));
+
+process.on('SIGTERM', () => {
+  server.close(() => {
+    console.log('Process terminated');
+  });
+});
